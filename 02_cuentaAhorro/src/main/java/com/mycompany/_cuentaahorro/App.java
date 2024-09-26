e241b18f0e38a89106b65354dc1471c74c143538
gjparts <gjparts@gmail.com>
2024-09-26 10:00:46 -0600

    commit 260920244639

--- 02_cuentaAhorro/src/main/java/com/mycompany/_cuentaahorro/App.java
+++ 02_cuentaAhorro/src/main/java/com/mycompany/_cuentaahorro/App.java
@@ -11,6 +11,14 @@ package com.mycompany._cuentaahorro;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        /*CuentaAhorro c1 = new CuentaAhorro("Gerardo", "11111111");
+        System.out.println("saldo de c1: "+c1.getSaldo());
+        c1.depositar(5500);
+        System.out.println("saldo de c1: "+c1.getSaldo());
+        c1.retirar(4000);
+        System.out.println("saldo de c1: "+c1.getSaldo());
+        c1.retirar(500);
+        System.out.println("saldo de c1: "+c1.getSaldo());*/
+        
     }
 }
