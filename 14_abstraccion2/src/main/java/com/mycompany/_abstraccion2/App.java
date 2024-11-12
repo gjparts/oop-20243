699e32d22f266acce7f6cd51036a15b2d5a49211
gjparts <gjparts@gmail.com>
2024-11-12 10:09:54 -0600

    commit 1211202429008

--- 14_abstraccion2/src/main/java/com/mycompany/_abstraccion2/App.java
+++ 14_abstraccion2/src/main/java/com/mycompany/_abstraccion2/App.java
@@ -1,16 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany._abstraccion2;
-
-/**
- *
- * @author Gerardo
- */
 public class App {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //Figura f1 = new Figura("Paralelogramo");
+        Rectangulo re1 = new Rectangulo(6, 5);
+        System.out.println("Area de re1: "+re1.calcularArea());
+        System.out.println("Perimetro de re1: "+re1.calcularPerimetro());
+        
+        Circulo c1 = new Circulo(80);
+        System.out.println("Area de c1: "+c1.calcularArea());
+        System.out.println("Perimetro de c1: "+c1.calcularPerimetro());
+        
+        TrianguloEquilatero t1 = new TrianguloEquilatero(15,10);
+        System.out.println("Area de t1: "+t1.calcularArea());
+        System.out.println("Perimetro de t1: "+t1.calcularPerimetro());
+        
+        
+        Figura f1 = new Figura("Cuadrado"){
+            //si se crean nuevos atributos/metodos de esta forma
+            //estos no estaran disponibles fuera de este ambito. (A)
+            public double lado = 4; //debera asignarles un valor aqui
+            //solo se puede implementar lo existente:
+            @Override
+            public double calcularArea() {
+                return Math.pow(lado, 2);
+            }
+            @Override
+            public double calcularPerimetro() {
+                return 4*lado;
+            }
+        };
+        //f1.lado = 5; //lo mencionado en (A)
+        System.out.println("Area de f1: "+f1.calcularArea());
+        System.out.println("Perimetro de f1: "+f1.calcularPerimetro());
+ 
     }
 }
