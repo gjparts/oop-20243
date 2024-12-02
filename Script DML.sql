6b1fffbb27a5a883f254978cac82ce0eee480a33
gjparts <gjparts@gmail.com>
2024-12-02 10:54:21 -0600

    commit 0212202411764

--- Script DML.sql
+++ Script DML.sql
@@ -178,3 +178,102 @@ WHERE PrecioVenta-Costo > 5
 SELECT * FROM Producto WHERE Comentarios IS NULL
 --productos cuyo comentario NO sea nulo
 SELECT * FROM Producto WHERE NOT Comentarios IS NULL
+
+--operador OR
+--productos con costo menor a 5 o arriba de 10
+SELECT * FROM Producto WHERE Costo < 5 OR Costo > 10
+
+--uso del DISTINTO DE
+--productos cuyo costo NO SEA igual a 25
+--forma 1:
+SELECT * FROM Producto WHERE Costo != 25
+--forma 2:
+SELECT * FROM Producto WHERE Costo <> 25
+--forma 3: usando el operador logico NOT
+SELECT * FROM Producto WHERE NOT Costo = 25
+
+--WHERE en campos de texto
+--Importante: algunos motores de base de datos ignorar mayusc/misnuc de forma predeterminada
+--esto va a depender de la forma en que ha sido configurado el servidor y la base de datos
+--productos cuyo codigo sea CC01
+SELECT * FROM Producto WHERE Codigo = 'CC01'
+--productos cuyos codigos sean CC01, AB50, AB01 y XY99
+SELECT * FROM Producto WHERE Codigo IN ('CC01','AB50','AB01','XY99')
+--productos cuyos codigos NO sean CC01, AB50, AB01 y XY99
+SELECT * FROM Producto WHERE NOT Codigo IN ('CC01','AB50','AB01','XY99')
+--IN tambien sirve con numeros
+--producto cuyo ProductoID sea 1,3,5,7,100
+SELECT * FROM Producto WHERE ProductoID IN (1,3,5,7,100)
+--uso del operador LIKE
+--productos que lleven la palabra salud dentro de sus comentarios en cualquier parte
+SELECT * FROM Producto WHERE Comentarios LIKE '%salud%'
+--productos que lleven la letra e en cualquier parte de su nombre
+SELECT * FROM Producto WHERE Nombre LIKE '%e%'
+--Productos que lleven la palabra salud en cualquier parte del comentario
+--y la letra a en cualquier parte de su nombre
+SELECT * FROM Producto WHERE Comentarios LIKE '%salud%' AND Nombre LIKE '%a%'
+--productos cuyo nombre comience con la letra c
+SELECT * FROM Producto WHERE Nombre LIKE 'c%'
+--productos cuyo nombre termine con la letra L
+SELECT * FROM Producto WHERE Nombre LIKE '%L'
+--productos cuyo codigo comience con CC
+SELECT * FROM Producto WHERE Codigo LIKE 'CC%'
+
+/*Instruccion DELETE
+Elimina los registros de una tabla
+-> IMPORTANTE: se recomienda combinarlo con WHERE para determinar lo que va a borrar.
+               un DELETE sin WHERE borra todos los registros de la tabla SIN reiniciar
+			   el campo IDENTITY.*/
+--borrar todos los registros de la tabla Producto
+DELETE FROM Producto
+--RECOMENDACION: antes de ejecutar un DELETE, es importante hacer un SELECT con el mismo
+--WHERE que va a usar en el DELETE, asi podra saber que registros se van a eliminar.
+--lo anterior equivale a hacer una vista previa de los registros a borrar.
+--eliminar todos los productos cuyo costo sea menor a 30
+SELECT * FROM Producto WHERE Costo < 30
+DELETE FROM Producto WHERE Costo < 30
+--eliminar los productos cuyo comentario sea nulo
+SELECT * FROM Producto WHERE Comentarios IS NULL
+DELETE FROM Producto WHERE Comentarios IS NULL
+
+--Instruccion TRUNCATE: borra todos los registros de una tabla, en el caso de SQL SERVER
+--reinicia el campo IDENTITY, TRUNCATE no soporta WHERE.
+TRUNCATE TABLE Producto
+
+/*Instruccion UPDATE
+Hace cambios en los registros de una tabla, se recomienda combinarlo con WHERE
+Al igual que DELETE tambien se recomiendo hacer una vista previa usando SELECT.*/
+--Colocar en CERO todos los costos de los productos
+SELECT * FROM Producto
+UPDATE Producto SET Costo = 0 
+--colocar en 60 el costo del producto con codigo CC01
+SELECT * FROM Producto WHERE Codigo = 'CC01'
+UPDATE Producto SET Costo = 60 WHERE Codigo = 'CC01'
+--colocar en 0 el costo de los productos con codigo CC01, CC02, VD01
+SELECT * FROM Producto WHERE Codigo IN ('CC01','CC02','VD01')
+UPDATE Producto SET Costo = 0 WHERE Codigo IN ('CC01','CC02','VD01')
+--UPDATE permite afectar mas de nun campo
+--colocar en 60 el costo y en 75 el precio de venta para los productos con codigos CC01, CC02, VD01
+SELECT * FROM Producto WHERE Codigo IN ('CC01','CC02','VD01')
+UPDATE Producto SET Costo = 60, PrecioVenta = 75 WHERE Codigo IN ('CC01','CC02','VD01')
+--tambien puede realizar calculos en los UPDATE
+--amentar en un 10% el precio de todos los productos
+SELECT * FROM Producto
+UPDATE Producto SET PrecioVenta = PrecioVenta*1.10
+--incrementar en 5 las existencias de los productos cuyo codigo comience con CC
+SELECT * FROM Producto WHERE Codigo LIKE 'CC%'
+UPDATE Producto SET Existencias += 5 WHERE Codigo LIKE 'CC%'
+--colocar el comentario PROMOCION en los registros cuyo comentario sea NULL
+SELECT * FROM Producto WHERE Comentarios IS NULL
+UPDATE Producto SET Comentarios = 'PROMOCION' WHERE Comentarios IS NULL
+--tambie puede concatenar texto en los campos
+--agregar la palabra REVISADO a todos los comentarios de aquellos productos con codigos: CC01 y CC02
+SELECT * FROM Producto WHERE Codigo IN ('CC01','CC02')
+UPDATE Producto SET Comentarios = CONCAT(Comentarios,' REVISADO')
+WHERE Codigo IN ('CC01','CC02')
+--aplicar un descuento del 15% al precio de venta de todos los productos
+--que tengan la palabra PROMOCION en sus comentarios
+SELECT * FROM Producto WHERE Comentarios LIKE '%PROMOCION%'
+UPDATE Producto SET PrecioVenta = PrecioVenta*0.85 WHERE Comentarios LIKE '%PROMOCION%'
+
+SELECT * FROM Producto
