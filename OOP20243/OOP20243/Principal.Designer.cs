f26a08545ae07b6cd73f6b561472b759f0ab8783
gjparts <gjparts@gmail.com>
2024-12-03 10:01:05 -0600

    commit 0312202421297

--- OOP20243/OOP20243/Principal.Designer.cs
+++ OOP20243/OOP20243/Principal.Designer.cs
@@ -28,13 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Principal));
+            this.pruebaConexion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pruebaConexion
+            // 
+            this.pruebaConexion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pruebaConexion.Location = new System.Drawing.Point(308, 175);
+            this.pruebaConexion.Name = "pruebaConexion";
+            this.pruebaConexion.Size = new System.Drawing.Size(165, 68);
+            this.pruebaConexion.TabIndex = 0;
+            this.pruebaConexion.Text = "Probar Conexión";
+            this.pruebaConexion.UseVisualStyleBackColor = true;
+            this.pruebaConexion.Click += new System.EventHandler(this.pruebaConexion_Click);
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(723, 378);
+            this.Controls.Add(this.pruebaConexion);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Principal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "20012002049 Gerardo";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button pruebaConexion;
     }
 }
 
--- OOP20243/OOP20243/Principal.cs
+++ OOP20243/OOP20243/Principal.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace OOP20243
 {
@@ -15,6 +16,25 @@ namespace OOP20243
         public Principal()
         {
             InitializeComponent();
+        }
+
+        private void pruebaConexion_Click(object sender, EventArgs e)
+        {
+            //prueba de conexion
+            try
+            {
+                //crear objeto de conexion a la base de datos
+                SqlConnection con = new SqlConnection(Properties.Resources.cadenaConexion);
+                //ejecutar una prueba de conexion al servidor: abrir conexion
+                con.Open();
+                //si llego aqui es porque la prueba exitosa
+                MessageBox.Show("Conexion exitosa");
+                //cerrar la conexion a la base de datos
+                con.Close();
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message); //mostrar mensaje en caso de error
+            }
         }
     }
 }
